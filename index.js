95189539270cc0ae495a6e64cd7f876b754f2a09
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2025-01-20 23:02:40 +0000

    Refactor handler functions. Fix DEL bug.

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const displayPanel = document.querySelector(".display-panel .result");
 const numbersBtns = document.querySelector(".numbers-buttons");
 const opsBtns = document.querySelector(".ops-buttons");
 
-let num1 = 0;
-let num2 = 0;
-let operator = null;
-let result = 0;
-
-let calculationStage = "first"; // [first, second, result]
+const state = {
+  num1: 0,
+  num2: 0,
+  operator: null,
+  result: 0,
+  calculationStage: "first",
+};
 
 function operate(num1, num2, operator) {
   switch (operator) {
@@ -26,75 +27,90 @@ function updateDisplay(number) {
   displayPanel.textContent = parseFloat(number);
 }
 
+// Event Listeners
+
 numbersBtns.addEventListener("click", (e) => {
   if (!e.target.tagName === "BUTTON") return;
-  const numberPressed = e.target.dataset.value;
+  handleNumberPress(e.target.dataset.value, state);
 
-  switch (calculationStage) {
-    case "first":
-      num1 += numberPressed;
-      updateDisplay(num1);
-      break;
-    case "second":
-      num2 += numberPressed;
-      updateDisplay(num2);
-      break;
-    case "result":
-      num1 = numberPressed;
-      calculationStage = "first";
-      updateDisplay(num1);
-      break;
-  }
-
-  console.table(num1, operator, num2, result, `(state: ${calculationStage})`);
+  console.table(state);
 });
 
 opsBtns.addEventListener("click", (e) => {
   if (e.target.classList.contains("ops-math")) {
-    if (calculationStage === "second") {
-      result = operate(num1, num2, operator);
-      num1 = result;
-      num2 = 0;
-      updateDisplay(result);
-    }
-    calculationStage = "second";
-    operator = e.target.id.split("-")[1];
+    handleOperatorPress(e, state);
   }
 
-  if (e.target.id === "btn-equal" && calculationStage === "second") {
-    result = operate(num1, num2, operator);
-    num1 = result;
-    num2 = 0;
-    calculationStage = "result";
-    updateDisplay(result);
+  if (e.target.id === "btn-equal" && state.calculationStage === "second") {
+    handleEqualPress(state);
   }
 
   if (e.target.id === "btn-del") {
-    if (calculationStage === "first" && num1) {
-      num1 = num1.slice(0, -1);
-      updateDisplay(num1);
-    } else if (calculationStage === "second" && num2) {
-      num2 = num2.slice(0, -1);
-      updateDisplay(num2);
-    }
+    handleDeletePress(state);
   }
 
   if (e.target.id === "btn-ac") {
-    num1 = 0;
-    num2 = 0;
-    operator = null;
-    result = 0;
-    calculationStage = "first";
-    updateDisplay(0);
-    console.clear();
+    handleAcPress(state);
   }
 
-  console.table(
-    num1,
-    operator,
-    num2,
-    "=",
-    result,
-    `(state: ${calculationStage})`
-  );
+  console.table(state);
 });
+
+// Event Handlers
+
+function handleNumberPress(number, state) {
+  switch (state.calculationStage) {
+    case "first":
+      state.num1 += number;
+      updateDisplay(state.num1);
+      break;
+    case "second":
+      state.num2 += number;
+      updateDisplay(state.num2);
+      break;
+    case "result":
+      state.num1 = number;
+      state.calculationStage = "first";
+      updateDisplay(state.num1);
+      break;
+  }
+}
+
+function handleOperatorPress(event, state) {
+  if (state.calculationStage === "second" && state.num2) {
+    state.result = operate(state.num1, state.num2, state.operator);
+    state.num1 = state.result;
+    state.num2 = 0;
+    updateDisplay(state.result);
+  }
+  state.calculationStage = "second";
+  state.operator = event.target.id.split("-")[1];
+}
+
+function handleEqualPress(state) {
+  state.result = operate(state.num1, state.num2, state.operator);
+  state.num1 = state.result;
+  state.num2 = 0;
+  state.calculationStage = "result";
+  updateDisplay(state.result);
+}
+
+function handleDeletePress(state) {
+  if (state.calculationStage === "first" && state.num1) {
+    state.num1 = state.num1.slice(0, -1) || 0;
+    updateDisplay(state.num1);
+  } else if (state.calculationStage === "second" && state.num2) {
+    state.num2 = state.num2.slice(0, -1) || 0;
+    updateDisplay(state.num2);
+  }
+}
+
+function handleAcPress(state) {
+  state.num1 = 0;
+  state.num2 = 0;
+  state.operator = null;
+  state.result = 0;
+  state.calculationStage = "first";
+  updateDisplay(0);
+  console.clear();
+}
